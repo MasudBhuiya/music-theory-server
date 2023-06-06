da044f44d04661ecaaf61810914ce15dfdb70e77
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-06-06 20:28:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Summer camp')
+  })
+
+app.listen(port, () => {
+    console.log(`Summer camp is running on port ${port}`)
+  })
