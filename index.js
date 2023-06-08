931433d97475784dc904dfc502c098f37e173e3b
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-06-08 11:05:14 +0600

    update class by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -31,9 +31,43 @@ async function run() {
 
 
     const classCollection = client.db('campDB').collection('class');
+    const instructorCollection = client.db('campDB').collection('instructor');
 
+    //classes
     app.get('/classes', async(req, res)=>{
-      const result = await classCollection.find().toArray();
+      const query = {}
+      const options = {
+        sort: { "enroll": -1 }
+      };
+      const result = await classCollection.find(query, options).toArray();
+      res.send(result)
+    })
+
+    app.put('/classes/:id', async(req, res)=>{
+      const id = req.params.id;
+      const update = req.body;
+      console.log(update)
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const classes = {
+        $set: {
+          status: update.status,
+          enroll: parseFloat(update.enroll),
+          availableSeats: parseFloat(update.availableSeats),
+        }
+      }
+      const result = await classCollection.updateOne(filter, classes, options);
+      res.send(result)
+    });
+
+
+
+
+    
+
+    //instructors
+    app.get('/instructors', async(req, res)=>{
+      const result = await instructorCollection.find().toArray();
       res.send(result)
     })
 
