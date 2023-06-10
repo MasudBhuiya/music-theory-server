b9e17844960264f00772a92f87b07177e5d74f29
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-06-10 20:21:59 +0600

    create admin or instructor

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(cors())
 app.use(express.json());
 
 
-//mongodb
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n2defbf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,12 +28,32 @@ async function run() {
     await client.connect();
 
 
-    const classCollection = client.db('campDB').collection('class');
-    const instructorCollection = client.db('campDB').collection('instructor');
-    const allClassCollection = client.db('campDB').collection('allClass');
+    const classCollection = client.db('summerDb').collection('class');
+    const instructorCollection = client.db('summerDb').collection('instructors');
+    const usersClassCollection = client.db('summerDb').collection('usersclass');
+    const usersCollection = client.db('summerDb').collection('users');
 
-    //classes
-    app.get('/classes', async(req, res)=>{
+    //users related apis
+    app.get('/users', async(req, res)=> {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await usersClassCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exists'})
+      }
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    });
+
+    
+    
+     //all classes
+    app.get('/class', async(req, res)=>{
       const query = {}
       const options = {
         sort: { "enroll": -1 }
@@ -43,15 +61,11 @@ async function run() {
       const result = await classCollection.find(query, options).toArray();
       res.send(result)
     })
-    app.get('/allclass', async(req, res)=>{
-      const result = await classCollection.find().toArray();
-      res.send(result)
-    })
-
-    app.put('/classes/:id', async(req, res)=>{
+    
+    app.put('/class/:id', async(req, res)=>{
       const id = req.params.id;
       const update = req.body;
-      // console.log(update)
+      console.log(update)
       const filter = {_id: new ObjectId(id)};
       const options = {upsert: true};
       const classes = {
@@ -66,22 +80,38 @@ async function run() {
     });
 
 
+
     //post my data by email
-    app.post('/allclasses', async(req, res)=>{
-      const allclasses = req.body;
-      const result = await allClassCollection.insertOne(allclasses);
+    app.post('/usersclass', async(req, res)=>{
+      const usersdata = req.body;
+      console.log(req.body)
+      const result = await usersClassCollection.insertOne(usersdata);
+      res.send(result)
+    });
+
+    app.get('/usersclass', async(req, res)=>{
+      const result = await usersClassCollection.find().toArray();
       res.send(result)
     })
  
-    //get my data by email
+    // get my data by email
     app.get('/myclasses', async(req, res)=>{
       let query = {};
       if(req.query?.email){
         query = {email: req.query.email}
       }
-      const result = await allClassCollection.find(query).toArray();
+      const result = await usersClassCollection.find(query).toArray();
       res.send(result)
     })
+
+//     //delete my class
+   app.delete('/usersclass/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await usersClassCollection.deleteOne(query);
+    res.send(result)
+   })
+
 
 
     //instructors
@@ -89,6 +119,13 @@ async function run() {
       const result = await instructorCollection.find().toArray();
       res.send(result)
     })
+
+    app.post('/instructors', async(req, res)=>{
+      const usersdata = req.body;
+      console.log(req.body)
+      const result = await instructorCollection.insertOne(usersdata);
+      res.send(result)
+    });
 
 
 
@@ -108,9 +145,147 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Summer camp')
+    res.send('Summer camps')
   })
 
 app.listen(port, () => {
     console.log(`Summer camp is running on port ${port}`)
   })
+
+
+//-------------------------------------------------------
+// const express = require('express');
+// require('dotenv').config();
+// const cors = require('cors');
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const app = express()
+// const port = process.env.PORT || 5000;
+
+
+// // middleware
+// app.use(cors())
+// app.use(express.json());
+
+
+// //mongodb
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n2defbf.mongodb.net/?retryWrites=true&w=majority`;
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+
+//     const classCollection = client.db('campDB').collection('class');
+//     const instructorCollection = client.db('campDB').collection('instructor');
+//     const allclassesCollection = client.db('campDB').collection('allclasses');
+//     const usersCollection = client.db('campDB').collection('userclass');
+    
+
+//     //classes
+//     app.get('/class', async(req, res)=>{
+//       const query = {}
+//       const options = {
+//         sort: { "enroll": -1 }
+//       };
+//       const result = await classCollection.find(query, options).toArray();
+//       res.send(result)
+//     })
+    
+
+//     app.put('/class/:id', async(req, res)=>{
+//       const id = req.params.id;
+//       const update = req.body;
+//       // console.log(update)
+//       const filter = {_id: new ObjectId(id)};
+//       const options = {upsert: true};
+//       const classes = {
+//         $set: {
+//           status: update.status,
+//           enroll: parseFloat(update.enroll),
+//           availableSeats: parseFloat(update.availableSeats),
+//         }
+//       }
+//       const result = await classCollection.updateOne(filter, classes, options);
+//       res.send(result)
+//     });
+
+
+//     //post my data by email
+//     app.post('/userclass', async(req, res)=>{
+//       const usersClasses = req.body;
+//       console.log(req)
+//       const result = await usersCollection.insertOne(usersClasses);
+//       res.send(result)
+//     });
+
+//     // app.get('/allclasses', async(req, res)=>{
+//     //   const result = await allclassesCollection.find().toArray();
+//     //   res.send(result)
+//     // })
+ 
+//     //get my data by email
+//     // app.get('/myclasses', async(req, res)=>{
+//     //   let query = {};
+//     //   if(req.query?.email){
+//     //     query = {email: req.query.email}
+//     //   }
+//     //   const result = await allClassCollection.find(query).toArray();
+//     //   res.send(result)
+//     // })
+
+
+
+//     //delete my class
+//     // app.delete('/allclasses/:id', async(req, res)=>{
+//     //   const id = req.params.id;
+//     //   console.log(id)
+//     //   const query = {_id : new ObjectId(id)};
+//     //   const result = await allClassCollection.deleteOne(query);
+//     //   res.send(result);
+//     // })
+
+
+
+
+    
+//     //instructors
+//     app.get('/instructors', async(req, res)=>{
+//       const result = await instructorCollection.find().toArray();
+//       res.send(result)
+//     })
+
+
+
+
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+
+
+// app.get('/', (req, res) => {
+//     res.send('Summer camp')
+//   })
+
+// app.listen(port, () => {
+//     console.log(`Summer camp is running on port ${port}`)
+//   })
