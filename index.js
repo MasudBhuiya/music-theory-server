8fae17c3e8dd2a558a2d07503522fd63595bc426
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-06-10 20:23:06 +0600

    create patch for create instructor

--- index.js
+++ index.js
@@ -50,6 +50,22 @@ async function run() {
       res.send(result)
     });
 
+    //create Instructor
+    app.patch('/users/instructor/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+    
+
+
     
     
      //all classes
